0060fa353a59247603dca04ebfa951faabcfc186
MayuRuru <aruizrull@protonmail.com>
2023-03-13 18:24:27 +0100

    Fizzbuzz kata solved

--- fizzbuzz/src/fizzbuzz.js
+++ fizzbuzz/src/fizzbuzz.js
@@ -1,3 +1,80 @@
 export default () => {
-  return false;
+  const numbers = [];
+
+  for (let value = 1; value <= 100; value++) {
+    numbers.push(transform(value));
+  }
+
+  return numbers;
+};
+
+const multipleOfThree = multipleOf(3);
+const multipleOfFive = multipleOf(5);
+const containsFive = containsDigit("5");
+const containsThree = containsDigit("3");
+
+function transform(value) {
+  if (isFizzbuzz(value)) {
+    return "Fizzbuzz";
+  }
+
+  if (isFizz(value)) {
+    return "Fizz";
+  }
+
+  if (isBuzz(value)) {
+    return "Buzz";
+  }
+
+  return value;
 }
+
+// extracted functions:
+
+function isFizzbuzz(value) {
+  /*  return (
+    (multipleOfThree(n) && multipleOfFive(n)) ||
+    (containsFive(n) && containsThree(n))
+  ); */
+  return isBuzz(value) && isFizz(value);
+}
+
+function isFizz(value) {
+  return multipleOfThree(value) || containsThree(value);
+}
+
+function isBuzz(value) {
+  return multipleOfFive(value) || containsFive(value);
+}
+
+/*  if (multipleOfFive(n) || value.toString().includes("5")) {
+  return "Buzz";
+} */
+
+// helper functions:
+
+function multipleOf(divisor) {
+  return function (value) {
+    return value % divisor == 0;
+  };
+}
+
+// Functional programming: multipleOf(3)(15);
+
+function containsDigit(digit) {
+  return function (value) {
+    return value.toString().includes(digit);
+  };
+}
+
+/* function containsThreeDigit(value) {
+  return value.toString().includes("5");
+} */
+
+/* function multipleOfThree(n) {
+  return n == 3;
+}
+
+function multipleOfFive(n) {
+  return n % 5 == 0;
+} */
--- fizzbuzz/src/fizzbuzz.test.js
+++ fizzbuzz/src/fizzbuzz.test.js
@@ -1,7 +1,62 @@
 import fizzbuzz from "./fizzbuzz";
 
-describe('Fizzbuzz', () => {
-  test("canary test", () => {
-    expect(fizzbuzz()).toEqual(true);
+describe("Fizzbuzz", () => {
+  let numbers;
+
+  test("First element is a 1", () => {
+    numbers = fizzbuzz();
+    expect(numberFor(1)).toEqual(1);
   });
+
+  test("Element 2 is 2", () => {
+    numbers = fizzbuzz();
+    expect(numberFor(2)).toBe(2);
+  });
+
+  describe("Multiples of three are Fizz", () => {
+    test.each([[3], [6]])("%i", (value) => {
+      numbers = fizzbuzz();
+      expect(numberFor(value)).toBe("Fizz");
+    });
+  });
+
+  test("Element 5 is Buzz", () => {
+    numbers = fizzbuzz();
+    expect(numberFor(5)).toBe("Buzz");
+  });
+
+  test("Element 13 is Fizz", () => {
+    numbers = fizzbuzz();
+    expect(numberFor(13)).toBe("Fizz");
+  });
+
+  test("Element 15 is FizzBuzz", () => {
+    numbers = fizzbuzz();
+    expect(numberFor(15)).toBe("Fizzbuzz");
+  });
+
+  test("element 51 is FizzBuzz", () => {
+    numbers = fizzbuzz();
+    expect(numberFor(51)).toBe("Fizzbuzz");
+  });
+
+  test("element 52 is Buzz", () => {
+    numbers = fizzbuzz();
+    expect(numberFor(52)).toBe("Buzz");
+  });
+
+  test("element 53 is FizzBuzz", () => {
+    numbers = fizzbuzz();
+    expect(numberFor(53)).toBe("Fizzbuzz");
+  });
+
+  test("it returns an array of 100 elements", () => {
+    numbers = fizzbuzz();
+    expect(numbers.length).toEqual(100);
+  });
+
+  // helper function:
+  function numberFor(position) {
+    return numbers[position - 1];
+  }
 });
