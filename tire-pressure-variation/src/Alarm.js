e551036499b65ba3a2281f69a0dfac69ae111898
MayuRuru <aruizrull@protonmail.com>
2023-03-23 10:32:07 +0100

    Refact Alarm to bring doubles as dependencies for constructor

--- tire-pressure-variation/src/Alarm.js
+++ tire-pressure-variation/src/Alarm.js
@@ -1,27 +1,37 @@
-import Sensor from "./Sensor";
+//import Sensor from "./Sensor";
 
 var alarmOn = Symbol();
-
 export default class Alarm {
-  constructor() {
+/*  constructor() {
     this.lowPressureThreshold = 17;
     this.highPressureThreshold = 21;
     this.sensor = new Sensor();
     this[alarmOn] = false;
+  }*/
+
+  constructor(sensor,notifier) {
+    this.lowPressureThreshold = 17;
+    this.highPressureThreshold = 21;
+    this.sensor = sensor;
+    this.notifier = notifier;
+    this[alarmOn] = false;
   }
 
   check() {
-    const psiPressureValue = this.sensor.popNextPressurePsiValue();
+    //const psiPressureValue = this.sensor.popNextPressurePsiValue();
+    const psiPressureValue = this.sensor.popNextValue();
 
     if (psiPressureValue < this.lowPressureThreshold || this.highPressureThreshold < psiPressureValue) {
       if(!this[alarmOn]) {
         this[alarmOn] = true;
-        console.log("Alarm activated!");
+        // console.log("Alarm activated!");
+        this.notifier.notifyActiveState();
       }
     } else {
       if(this[alarmOn]) {
         this[alarmOn] = false;
-        console.log("Alarm deactivated!");
+        // console.log("Alarm deactivated!");
+        this.notifier.notifyInactiveState();
       }
     }
   }
