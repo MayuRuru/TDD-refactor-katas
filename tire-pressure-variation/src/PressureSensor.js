f7f913563b1caef31aeab92d137f894f6d41be57
MayuRuru <aruizrull@protonmail.com>
2023-03-23 12:17:04 +0100

    Remove unused

--- tire-pressure-variation/src/PressureSensor.js
+++ tire-pressure-variation/src/PressureSensor.js
@@ -1,17 +1,17 @@
 function offset() {
   return 16; 
-};
+}
 
 function samplePressure() {
   // placeholder implementation that simulate a real sensor in a real tire
   /*  const pressureTelemetryValue = Math.floor(6 * Math.random() * Math.random());
     return pressureTelemetryValue;*/
   return Math.floor(6 * Math.random() * Math.random());
-};
+}
 
 export default class PressureSensor {
   popNextValue() {
-    var pressureTelemetryValue = samplePressure();
+    const pressureTelemetryValue = samplePressure();
     return offset() + pressureTelemetryValue;
   }
 } 
