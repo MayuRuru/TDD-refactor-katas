9ff8d6152931ad44e5b60dbb3924a1a4a29ffa8a
MayuRuru <aruizrull@protonmail.com>
2023-03-23 12:50:06 +0100

    Remove unused after refact

--- tire-pressure-variation/src/Alarm.test.js
+++ tire-pressure-variation/src/Alarm.test.js
@@ -13,17 +13,11 @@ describe('Alarm', () => {
   });
 
   test("Alarm notifies if pressure is below threshold", () => {
-
-    //const sensor = {popNextValue: jest.fn()};
     sensor.popNextValue.mockReturnValue(16);
 
-    //const notifier = {notifyActiveState: jest.fn()};
-
-    //const alarm = new Alarm(sensor, notifier);
     alarm.check();
 
     expect(notifier.notifyActiveState).toHaveBeenCalledTimes(1);
   });
-
 
 });
