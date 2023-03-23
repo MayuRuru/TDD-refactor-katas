fc60065730b4ea9b7b63b41a8e9ce95574f8280f
MayuRuru <aruizrull@protonmail.com>
2023-03-23 12:40:21 +0100

    Mock Stub sensor and Spy notifier and pass test

--- tire-pressure-variation/src/Alarm.test.js
+++ tire-pressure-variation/src/Alarm.test.js
@@ -1,18 +1,20 @@
 import Alarm from "./Alarm";
 import PressureSensor from "./PressureSensor";
 import ConsoleNotifier from "./ConsoleNotifier";
-
 describe('Alarm', () => {
-  xtest("fixme", () => {
-    const alarm = new Alarm();
-    alarm.check();
-    expect(false).toEqual(false);
-  });
 
   test("Alarm notifies if pressure is below threshold", () => {
-    const alarm = new Alarm(new PressureSensor(), new ConsoleNotifier());
+
+    const sensor = {popNextValue: jest.fn()};
+    sensor.popNextValue.mockReturnValue(16);
+
+    const notifier = {notifyActiveState: jest.fn()};
+
+    const alarm = new Alarm(sensor, notifier);
     alarm.check();
-    expect(false).toEqual(false);
+
+    //expect(false).toEqual(false);
+    expect(notifier.notifyActiveState).toHaveBeenCalledTimes(1);
   });
 
 
