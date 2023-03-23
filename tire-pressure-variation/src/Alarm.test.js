86859c50833b87b1dcfbe5740a19e50d2ebdca80
MayuRuru <aruizrull@protonmail.com>
2023-03-23 12:46:27 +0100

    Extract sensor, notifier and alarm to beforeEach

--- tire-pressure-variation/src/Alarm.test.js
+++ tire-pressure-variation/src/Alarm.test.js
@@ -1,19 +1,27 @@
 import Alarm from "./Alarm";
-import PressureSensor from "./PressureSensor";
-import ConsoleNotifier from "./ConsoleNotifier";
+
 describe('Alarm', () => {
+
+  let sensor;
+  let notifier;
+  let alarm;
+
+  beforeEach(()=>{
+    sensor = {popNextValue: jest.fn()};
+    notifier = {notifyActiveState: jest.fn()};
+    alarm = new Alarm(sensor, notifier);
+  });
 
   test("Alarm notifies if pressure is below threshold", () => {
 
-    const sensor = {popNextValue: jest.fn()};
+    //const sensor = {popNextValue: jest.fn()};
     sensor.popNextValue.mockReturnValue(16);
 
-    const notifier = {notifyActiveState: jest.fn()};
+    //const notifier = {notifyActiveState: jest.fn()};
 
-    const alarm = new Alarm(sensor, notifier);
+    //const alarm = new Alarm(sensor, notifier);
     alarm.check();
 
-    //expect(false).toEqual(false);
     expect(notifier.notifyActiveState).toHaveBeenCalledTimes(1);
   });
 
