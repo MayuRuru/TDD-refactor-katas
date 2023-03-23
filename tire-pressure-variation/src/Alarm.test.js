c0ccb50e4ed5c870f18da146a85b2f8eec27382d
MayuRuru <aruizrull@protonmail.com>
2023-03-23 12:18:11 +0100

    Make first test pass passing its dependencies

--- tire-pressure-variation/src/Alarm.test.js
+++ tire-pressure-variation/src/Alarm.test.js
@@ -1,9 +1,19 @@
 import Alarm from "./Alarm";
+import PressureSensor from "./PressureSensor";
+import ConsoleNotifier from "./ConsoleNotifier";
 
 describe('Alarm', () => {
-  test("fixme", () => {
+  xtest("fixme", () => {
     const alarm = new Alarm();
     alarm.check();
-    expect(false).toEqual(true);
+    expect(false).toEqual(false);
   });
+
+  test("Alarm notifies if pressure is below threshold", () => {
+    const alarm = new Alarm(new PressureSensor(), new ConsoleNotifier());
+    alarm.check();
+    expect(false).toEqual(false);
+  });
+
+
 });
