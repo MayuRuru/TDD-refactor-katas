a80a2190a528597ea6146ecbf82cfa9cb8c4dfde
MayuRuru <aruizrull@protonmail.com>
2023-03-23 13:15:20 +0100

    More tests

--- tire-pressure-variation/src/Alarm.test.js
+++ tire-pressure-variation/src/Alarm.test.js
@@ -25,7 +25,30 @@ describe('Alarm', () => {
     alarm.check();
 
     expect(notifier.notifyActiveState).toHaveBeenCalledTimes(1);
+  })
 
+  test("Alarm deactivates if pressure is back to normal range", ()=> {
+    sensor.popNextValue.mockReturnValueOnce(22).mockReturnValueOnce(21);
+
+    alarm.check();
+
+    expect(notifier.notifyActiveState).toHaveBeenCalledTimes(1);
+  })
+
+  test("Alarm does not notify if a changing pressure value is within range", ()=> {
+    sensor.popNextValue.mockReturnValueOnce(18).mockReturnValueOnce(17);
+
+    alarm.check();
+
+    expect(notifier.notifyActiveState).toHaveBeenCalledTimes(0);
+  })
+
+  xtest("Alarm already notifiyin out of range does not notify again if a changing pressure value is still out of range", ()=> {
+    sensor.popNextValue.mockReturnValueOnce(23).mockReturnValueOnce(25);
+
+    alarm.check();
+
+    expect(notifier.notifyActiveState).toHaveBeenCalledTimes(0);
   })
 
 });
